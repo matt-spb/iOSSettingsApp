16bf59af29db0f618e481093cdc85b4321d935b5
Matvey <mnf89@mail.ru>
2021-11-17 01:38:23 +0300

    initial commit

---
+++ iOS3-HW12-Fedyshin Matvey/Sources/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  iOS3-HW12-Fedyshin Matvey
+//
+//  Created by matt_spb on 17.11.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
+
