76963e21a8bd4cf2edaea17242eb4cd03be3e986
Matvey <mnf89@mail.ru>
2021-11-17 01:11:09 +0300

    Initial Commit

---
+++ iOS3-HW12-Fedyshin Matvey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS3-HW12-Fedyshin Matvey
+//
+//  Created by matt_spb on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
