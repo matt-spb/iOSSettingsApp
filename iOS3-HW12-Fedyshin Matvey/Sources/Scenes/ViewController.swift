a4942c35264643d8fd45ce5b7314adb786604461
Matvey <mnf89@mail.ru>
2021-11-17 03:06:40 +0300

    add table view and nav controller

--- iOS3-HW12-Fedyshin Matvey/Sources/Scenes/ViewController.swift
+++ iOS3-HW12-Fedyshin Matvey/Sources/Scenes/ViewController.swift
@@ -9,12 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView = UITableView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemPink
+        setupTableView()
+        view.backgroundColor = .white
+    }
+
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.pin(to: view)
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = UITableViewCell()
+        cell.accessoryType = .detailButton
+        cell.textLabel?.text = "Hello"
+        cell.detailTextLabel?.text = "World"
+        cell.imageView?.image = UIImage(named: "settings")
+
+
+        return cell
     }
 
 
 }
-
