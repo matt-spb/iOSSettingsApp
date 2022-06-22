1384195fd0d82b217f14b21de535d4e75dd47821
Matvey Fedyshin <iosdevelopermatt@gmail.com>
2022-06-22 21:25:55 +0300

    clear mvc

--- iOS3-HW12-Fedyshin Matvey/Sources/Scenes/Controller/ViewController.swift
+++ iOS3-HW12-Fedyshin Matvey/Sources/Scenes/Controller/ViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var model = [Section]()
+    var model: SettingsModel?
 
     private var settingsView: SettingsView? {
         guard isViewLoaded else { return nil }
@@ -15,39 +15,10 @@ class ViewController: UIViewController {
     }
 
     func configure() {
+        model = SettingsModel()
         view = SettingsView()
         title = "Settings"
-
-        settingsView?.tableView.delegate = self
-        settingsView?.tableView.dataSource = self
-        model = SettingsModel.createModels()
-    }
-}
-
-extension ViewController: UITableViewDataSource {
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return model.count
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return model[section].options.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let model = model[indexPath.section].options[indexPath.row]
-        guard let settingsView = settingsView else { return UITableViewCell() }
-        let cell = settingsView.getCell(for: model)
-        return cell
-    }
-}
-
-extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-
-        let model = model[indexPath.section].options[indexPath.row]
-        print("Нажата ячейка \(model.title)")
+        guard let models = model?.createModels() else { return }
+        settingsView?.configure(with: models)
     }
 }
